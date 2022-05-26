d0323744c0b8b81f974ad0681145c184bb9d9ade
Hugues-Pacôme Stock <huguesstock@gmail.com>
2022-05-26 16:05:08 +0200

    Co-authored-by: Yoann Chhang <YoannChhang@users.noreply.github.com>

--- cell.h
+++ cell.h
@@ -1,5 +1,15 @@
-struct Cell
+typedef struct
 {
     int position[2];
     int value;
-};
+    int color; // Value of the color (0 -> Red, 1 -> Blue, ect)
+    int path; // Value of path (0 -> start, 1 -> nextCorrect)
+
+} Cell;
+
+/* Cell** createArray(short** size){
+
+    Cell** arr;
+    
+
+} */
--- main.c
+++ main.c
@@ -1,38 +1,63 @@
 #include "cell.h"
-
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 
 
 int load_level()
 {
-    FILE *pf;
-    char* buff;
-    pf = fopen ("levels/1.dat", "r");
-    if (pf == NULL)
-        return 0;
-
+    FILE* ptr;
     
+    //get size of levels
+    int width = 0, height = 0;
+    char temp;
+    ptr = fopen("levels/1.dat", "r");
+    
+    while(temp != '\n'){
+        temp = fgetc(ptr);
+        if (isdigit(temp) && width == 0 && height == 0) {
+            width = temp-'0';
+            continue;
+        };
+        if (isdigit(temp) && width != 0 && height == 0) {
+            height += temp-'0';
+            continue;
+        };
+    }
+
+    int i;
+    int j;
+
+    int** mat=malloc(height*sizeof(int*)); 
+    for(i=0;i<width;++i){
+        mat[i]=malloc(4*sizeof(int));
+    }
 
 
-    fclose (pf); 
-    return 1;
+    FILE *file;
+    file=fopen("levels/1.dat", "r");
+
+    for(i = 0; i < 5; i++)
+    {
+        for(j = 0; j < 5; j++) 
+        {
+    //Use lf format specifier, %c is for character
+        if (!fscanf(file, "%d", &mat[i][j])) 
+            break;
+        // mat[i][j] -= '0'; 
+        printf("%d\n",mat[i][j]); //Use lf format specifier, \n is for new line
+        }
+
+    }
+    fclose(file);
+    return 0;
 }
 
 int main() {
 
     int length = 10;
-    int** currentLevel = malloc(10 * sizeof(struct Cell));
 
-    FILE *fin;
+    load_level();
 
-    fin = fopen( "levels/1.dat", "r" );
-    if( fin == NULL )
-        return 1;
-
-    printf( "Character count: %d.\n", get_length_from_file_line( fin ) );
-
-    fclose( fin );
     return 0;
 }
